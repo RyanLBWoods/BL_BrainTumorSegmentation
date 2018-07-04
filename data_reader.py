ca1e60bd9a5fa0eb8f1fba46241e3b83454e3d27
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-04 13:51:31 +0100

    get path

--- data_reader.py
+++ data_reader.py
@@ -4,3 +4,19 @@
 # @Author  : Bin LIN
 # Copyright (c) 2017-2018 University of St. Andrews, UK. All rights reserved
 #
+
+import os
+import nibabel as nib
+
+hgg_folder = 'MICCAI_BraTS_2018_Data_Training/HGG'
+lgg_folder = 'MICCAI_BraTS_2018_Data_Training/LGG'
+
+hgg_files = os.listdir(hgg_folder)
+hgg_volumes = []
+for hgg_file in hgg_files:
+    hgg_volume = os.listdir(os.path.join(hgg_folder, hgg_file))
+    for hgg_data in hgg_volume:
+        hgg = os.path.join(hgg_folder, hgg_file, hgg_data)
+        hgg_volumes.append(hgg)
+
+print(hgg_volumes)
