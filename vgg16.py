2b0df8ae31218a6385490148ecdb2f486653a4dd
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-05 11:43:56 +0100

    vgg net

---
+++ vgg16.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+# @Date    : 2018/7/4
+# @Author  : Bin LIN
+# Copyright (c) 2017-2018 University of St. Andrews, UK. All rights reserved
+#
+
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Input, ZeroPadding2D, Dropout, Conv2DTranspose, Cropping2D, Add
+
+
+def vgg_model():
+    channels, height, weight = 3, 500, 500
+
+    # Input
+    input_shape = (height, weight, 3)
+    img_input = Input(shape=input_shape)
+
+    # Add plenty of zero padding
+    x = ZeroPadding2D(padding=(100, 100))(img_input)
+
+    # VGG-16 convolution block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv1_1')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(x)
+
+    # VGG-16 convolution block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool2', padding='same')(x)
+
+    # VGG-16 convolution block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool3', padding='same')(x)
+    pool3 = x
+
+    # VGG-16 convolution block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool4', padding='same')(x)
+    pool4 = x
+
+    # VGG-16 convolution block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool5', padding='same')(x)
+
+    # Fully-connected layers converted to convolution layers
+    x = Conv2D(4096, (7, 7), activation='relu', padding='valid', name='fc6')(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(4096, (1, 1), activation='relu', padding='valid', name='fc7')(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(21, (1, 1), padding='valid', name='score-fr')(x)
+
+    # Deconvolution
+    score2 = Conv2DTranspose(21, (4, 4), strides=2, name='score2')(x)
+
+    # Skip connections from pool4
+    score_pool4 = Conv2D(21, (1, 1), name='score-pool4')(pool4)
+    score_pool4c = Cropping2D((5, 5))(score_pool4)
+    score_fused = Add()([score2, score_pool4c])
+    score4 = Conv2DTranspose(21, (4, 4), strides=2, name='score4', use_bias=False)(score_fused)
+
+    # Skip connections from pool3
+    score_pool3 = Conv2D(21, (1, 1), name='score-pool3')(pool3)
+    score_pool3c = Cropping2D((9, 9))(score_pool3)
+
+    # Fuse things together
+    score_final = Add()([score4, score_pool3c])
+
+    # Final up-sampling and cropping
+    upsample = Conv2DTranspose(21, (16, 16), strides=8, name='upsample', use_bias=False)(score_final)
+    upscore = Cropping2D(((31, 37), (31, 37)))(upsample)
+    
