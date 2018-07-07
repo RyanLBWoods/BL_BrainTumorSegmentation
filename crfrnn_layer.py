11bd73da87298a8ab4c9918888484190b7a1ec25
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-07 15:45:03 +0100

    change variables name

--- crfrnn_layer.py
+++ crfrnn_layer.py
@@ -12,6 +12,7 @@ import pydensecrf.densecrf as dcrf
 
 n_classes = 4
 
+
 def dense_crf(probs, img=None, n_iters=10, sxy_gaussian=(1, 1), compat_gaussian=4, kernel_gaussian=dcrf.DIAG_KERNEL,
               norm_gaussian=dcrf.NORMALIZE_SYMMETRIC, sxy_bilateral=(49, 49), compat_bilateral=5,
               srgb_bilateral=(13, 13, 13), kernel_bilateral=dcrf.DIAG_KERNEL, norm_bilateral=dcrf.NORMALIZE_SYMMETRIC):
@@ -20,9 +21,9 @@ def dense_crf(probs, img=None, n_iters=10, sxy_gaussian=(1, 1), compat_gaussian=
     probs = probs[0].transpose(2, 0, 1).copy(order='C')
 
     d = dcrf.DenseCRF2D(w, h, n_classes)
-    U = -np.log(probs)
-    U = U.reshap((n_classes, -1))
-    d.setUnaryEnergy(U)
+    unaries = -np.log(probs)
+    unaries = unaries.reshap((n_classes, -1))
+    d.setUnaryEnergy(unaries)
     d.addPairwiseGaussian(sxy=sxy_gaussian, compat=compat_gaussian, kernel=kernel_gaussian, normalization=norm_gaussian)
     if img is not None:
         d.addPairwiseBilateral(sxy=sxy_bilateral, compat=compat_bilateral, kernel=kernel_bilateral,
