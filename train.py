07f04340e2916562829728e11af78f2f72c2ccab
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-08 16:58:15 +0100

    add training functions

--- train.py
+++ train.py
@@ -14,27 +14,58 @@ import time
 import tensorflow as tf
 import numpy as np
 
-from ResNet import ResNet
+from ResNet import ResNetModel
 from data_reader import ScanReader
+from utils import decode_labels, prepare_label, inverse_preprocess
 
+n_classes = 4
 BATCH_SIZE = 4
 TRAINING_DATA_DIRECTORY = 'MICCAI_BraTS_2018_Data_Training'
 INPUT_SIZE = '240, 155'
 LEARNING_RATE = 1e-4
 NUM_STEPS = 20000
-
+RESTORE_FROM = './deeplab_resnet.ckpt'
+SAVE_NUM_IMAGES = 2
+SAVE_PRED_EVERY = 100
+SNAPSHOT_DIR = './snapshots/'
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='ResNet')
-    parser.add_argument("--batch-size", type=int, default=BATCH_SIZE, help='Number of scans sent to network in one step.')
+    parser.add_argument("--batch-size", type=int, default=BATCH_SIZE,
+                        help='Number of scans sent to network in one step.')
     parser.add_argument("--data-dir", type=str, default=TRAINING_DATA_DIRECTORY, help='Path to BraTS2018 training set.')
-    parser.add_argument("--input-size", type=str, default=INPUT_SIZE, help='Comma-separated string with height and width of images')
-    parser.add_argument("--is-training", action='store_true', help='Wether to update the running means and variances during training')
+    parser.add_argument("--input-size", type=str, default=INPUT_SIZE,
+                        help='Comma-separated string with height and width of images')
+    parser.add_argument("--is-training", action='store_true',
+                        help='Wether to update the running means and variances during training')
     parser.add_argument("--learning-rate", type=float, default=LEARNING_RATE, help='Learning rate for training')
-    parser.add_argument("--random-scale", action='store_true', help='Whether to randomly scale the inputs during training')
+    parser.add_argument("--num-steps", type=int, default=NUM_STEPS, help='Number of training steps')
+    parser.add_argument("--random-scale", action='store_true',
+                        help='Whether to randomly scale the inputs during training')
+    parser.add_argument("--restore-from", type=str, default=RESTORE_FROM, help="Where restore model parameters from.")
+    parser.add_argument("--save-num-images", type=int, default=SAVE_NUM_IMAGES, help="How many images to save.")
+    parser.add_argument("--save-pred-every", type=int, default=SAVE_PRED_EVERY,
+                        help="Save summaries and checkpoint every often.")
+    parser.add_argument("--snapshot-dir", type=str, default=SNAPSHOT_DIR, help="Where to save snapshots of the model.")
 
     return parser.parse_args()
+
+
+def save(saver, sess, logdir, step):
+    model_name = 'model.ckpt'
+    checkpoint_path = os.path.join(logdir, model_name)
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    saver.save(sess, checkpoint_path, global_step=step)
+    print('The checkpoint has been created')
+
+
+def load(saver, sess, ckpt_path):
+    saver.restore(sess, ckpt_path)
+    print("Restored model parameters from {}".format(ckpt_path))
 
 
 def main():
@@ -50,4 +81,74 @@ def main():
         scan_batch, label_batch = scan_reader.dequeue(args.batch_size)
 
     # Build neural net
-    net = ResNet({'data': scan_batch}, is_training=args.is_training)
+    net = ResNetModel({'data': scan_batch}, is_training=args.is_training)
+
+    # Predictions
+    output = net.layers['fc_voc12']
+    restore_var = tf.global_variables()
+    trainable = tf.trainable_variables()
+
+    prediction = tf.reshape(output, [-1, n_classes])
+    label_proc = prepare_label(label_batch, tf.pack(output.get_shape()[1: 3, ]))
+    gt = tf.reshape(label_proc, [-1, n_classes])
+
+    # Pixel-wise softmax loss
+    loss = tf.nn.softmax_cross_entropy_with_logits(output, tf.shape(scan_batch)[1:3, ])
+    reduced_loss = tf.reduce_mean(loss)
+
+    # Process predictions
+    output_up = tf.image.resize_bilinear(output, tf.shape(scan_batch)[1:3, ])
+    output_up = tf.argmax(output_up, dimension=1)
+    pred = tf.expand_dims(output_up, dim=1)
+
+    # Image summary
+    scans_summary = tf.py_func(inverse_preprocess, [scan_batch, args.save_num_images], tf.uint8)
+    labels_summary = tf.py_func(decode_labels, [label_batch, args.save_num_images], tf.uint8)
+    preds_summary = tf.py_func(decode_labels, [pred, args.save_num_images], tf.uint8)
+
+    total_summary = tf.summary.image('images', tf.concat(2, [scans_summary, labels_summary, preds_summary]),
+                                     max_outputs=args.save_num_images)
+    summary_writer = tf.summary.FileWriter(args.snapshot_dir)
+
+    # Define loss and optimisation parameters
+    optimiser = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
+    optim = optimiser.minimize(reduced_loss, var_list=trainable)
+
+    # Set up tf session and initialize variables
+    config = tf.ConfigProto()
+    config.gpu_option.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.global_variables_initializer()
+
+    sess.run(init)
+
+    # Saver for storing checkpoints of the model
+    saver = tf.train.Saver(var_list=restore_var, max_to_keep=40)
+
+    # Load variables if the checkpoint is provided
+    if args.restore_from is not None:
+        loader = tf.train.Saver(var_list=restore_var)
+        load(loader, sess, args.restore_from)
+
+    # Start queue threads
+    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+    # Iterate over training steps
+    for step in range(args.num_steps):
+        start_time = time.time()
+
+        if step % args.save_pred_every == 0:
+            loss_value, scans, labels, preds, summary, _ = sess.run(
+                [reduced_loss, scan_batch, label_batch, pred, total_summary, optim])
+            summary_writer.add_summary(summary, step)
+            save(saver, sess, args.snapshot_dir, step)
+        else:
+            loss_value, _ = sess.run([reduced_loss, optim])
+        duration = time.time() - start_time
+        print('step {:d} \t loss = {:.3f}, ({:.3f} sec/step)'.format(step, loss_value, duration))
+    coord.request_stop()
+    coord.join(threads)
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -6,11 +6,44 @@
 #
 
 import numpy as np
+from PIL import Image
 import tensorflow as tf
 # from keras.engine.topology import Layer
 import pydensecrf.densecrf as dcrf
 
 n_classes = 4
+# image mean
+IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
+
+
+def decode_labels(label, num_images):
+    n, h, w, c = label.shape
+    assert (n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (
+        n, num_images)
+    outputs = np.zeros((num_images, h, w, 1), dtype=np.uint8)
+    for i in range(num_images):
+        img = Image.new('L', (h, w))
+        pixels = img.load()
+        # for j_, j in enumerate()
+        outputs[i] = np.array(img)
+    return outputs
+
+
+def prepare_label(input_batch, new_size):
+    with tf.name_scope('label_encode'):
+        input_batch = tf.squeeze(input_batch, squeeze_dims=[1])
+
+    return input_batch
+
+
+def inverse_preprocess(imgs, num_images):
+    n, h, w, c = imgs.shape
+    assert (n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (
+        n, num_images)
+    outputs = np.zeros((num_images, h, w, 1), dtype=np.uint8)
+    for i in range(num_images):
+        outputs[i] = (imgs[i] + IMG_MEAN)[:, :, ::-1].astype(np.uint8)
+    return outputs
 
 
 def dense_crf(probs, img=None, n_iters=10, sxy_gaussian=(1, 1), compat_gaussian=4, kernel_gaussian=dcrf.DIAG_KERNEL,
@@ -27,7 +60,7 @@ def dense_crf(probs, img=None, n_iters=10, sxy_gaussian=(1, 1), compat_gaussian=
     d.addPairwiseGaussian(sxy=sxy_gaussian, compat=compat_gaussian, kernel=kernel_gaussian, normalization=norm_gaussian)
     if img is not None:
         d.addPairwiseBilateral(sxy=sxy_bilateral, compat=compat_bilateral, kernel=kernel_bilateral,
-                               normalization=norm_bilateral, srgb=sxy_bilateral, rgbim=img[0])
+                               normalization=norm_bilateral, srgb=srgb_bilateral, rgbim=img[0])
     Q = d.inference(n_iters)
     preds = np.array(Q, dtype=np.float32).reshape((n_classes, h, w)).transpose(1, 2, 0)
     return np.expand_dims(preds, 0)
