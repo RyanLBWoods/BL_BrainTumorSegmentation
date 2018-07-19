da2d42991e0211e8d620312760b02bcbf3a0427d
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-19 10:55:56 +0100

    process whole tumor

---
+++ label_processing.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# @Date    : 2018/7/19
+# @Author  : Bin LIN
+# Copyright (c) 2017-2018 University of St. Andrews, UK. All rights reserved
+#
+
+import os
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_scans_list(folder):
+    seg_list = []
+    grades = ['HGG', 'LGG']
+    for grade in grades:
+        files = os.listdir(os.path.join(folder, grade))
+        for file in files:
+            volume = os.listdir(os.path.join(folder, grade, file))
+            for data in volume:
+                data_path = os.path.join(folder, grade, file, data)
+                if 'seg.nii' in data_path:
+                    seg_list.append(data_path)
+    return seg_list
+
+
+data_folder = 'MICCAI_BraTS_2018_Data_Training'
+
+gt_path = load_scans_list(data_folder)
+print(len(gt_path))
+
+
+def whole_tumor_label(gt_path_list):
+    whole_tumor_label_list = []
+    for path in gt_path_list:
+        print("processing", path)
+        seg_data = nib.load(path).get_data()
+        shape = seg_data.shape
+        patient_gt = []
+        for slice in seg_data:
+            processed_data = np.zeros(shape=(shape[1], shape[2]))
+            for row in range(len(slice)):
+                for col in range(len(slice[row])):
+                    if slice[row][col] != 0:
+                        processed_data[row][col] = 1
+            patient_gt.append(processed_data)
+        whole_tumor_label_list.append(patient_gt)
+    whole_tumor_label_list = np.array(whole_tumor_label_list)
+    return whole_tumor_label_list
+
+
+whole_tumor = whole_tumor_label(gt_path)
+print(whole_tumor.shape)
+
+for patient in whole_tumor:
+    for d in patient:
+        plt.imshow(d, cmap='gray')
+        plt.show()
