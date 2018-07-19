fc9f17d19e971bf447d3551905617270277550ba
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-19 11:57:30 +0100

    process sub-regions

--- label_processing.py
+++ label_processing.py
@@ -33,27 +33,41 @@ print(len(gt_path))
 
 def whole_tumor_label(gt_path_list):
     whole_tumor_label_list = []
+    tumor_core_label_list = []
+    cystic_label_list = []
     for path in gt_path_list:
         print("processing", path)
         seg_data = nib.load(path).get_data()
         shape = seg_data.shape
-        patient_gt = []
+        whole_tumor_gt = []
+        tumor_core_gt = []
+        cystic_gt = []
         for slice in seg_data:
-            processed_data = np.zeros(shape=(shape[1], shape[2]))
+            whole_tumor_data = np.zeros(shape=(shape[1], shape[2]))
+            tumor_core_data = np.zeros(shape=(shape[1], shape[2]))
+            # enhancing_tumor = np.zeros(shape=(shape[1], shape[2]))
+            cystic_data = np.zeros(shape=(shape[1], shape[2]))
             for row in range(len(slice)):
                 for col in range(len(slice[row])):
                     if slice[row][col] != 0:
-                        processed_data[row][col] = 1
-            patient_gt.append(processed_data)
-        whole_tumor_label_list.append(patient_gt)
+                        whole_tumor_data[row][col] = 1
+                        if slice[row][col] != 2:
+                            tumor_core_data[row][col] = 1
+                            if slice[row][col] != 4:
+                                cystic_data[row][col] = 1
+
+            whole_tumor_gt.append(whole_tumor_data)
+            tumor_core_gt.append(tumor_core_data)
+            cystic_gt.append(cystic_data)
+
+        whole_tumor_label_list.append(whole_tumor_gt)
+        tumor_core_label_list.append(tumor_core_gt)
+        cystic_label_list.append(cystic_gt)
+        break
     whole_tumor_label_list = np.array(whole_tumor_label_list)
-    return whole_tumor_label_list
+    tumor_core_label_list = np.array(tumor_core_label_list)
+    cystic_label_list = np.array(cystic_label_list)
+    return whole_tumor_label_list, tumor_core_label_list, cystic_label_list
 
 
-whole_tumor = whole_tumor_label(gt_path)
-print(whole_tumor.shape)
-
-for patient in whole_tumor:
-    for d in patient:
-        plt.imshow(d, cmap='gray')
-        plt.show()
+whole_tumor, tumor_core, cystic = whole_tumor_label(gt_path)
