dfcd30455b7a0f3f0888859c79a776125abc7eb0
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-07-25 16:20:15 +0100

    try training with one patient's data

--- train2d.py
+++ train2d.py
@@ -6,9 +6,16 @@
 #
 
 import argparse
+import json
 import tensorflow as tf
 from ResNet2D import ResnetBuilder
+from vgg16 import vgg_model
+import vgg16
+import UNet
 from load_data_2d import ScanReader
+from keras.utils import to_categorical
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 n_classes = 4
 BATCH_SIZE = 4
@@ -53,29 +60,108 @@ def load_data():
 
 
 def main():
+    # l = np.random.randint(10, size=(1000, 1))
+    # print(l)
+    # print(l.shape)
+    # one_hot_l = to_categorical(l, num_classes=10)
+    # print(one_hot_l)
+    # exit(0)
+    # Build ResNet-101 model
+    model = ResnetBuilder.build_resnet_101((240, 155, 1), 2)
+    # unet = UNet.UNet3D((240, 240, 155, 1))
+    # vgg_model = vgg16.vgg_model()
+    print("Model Built")
+
+    # Compiling
+    model.compile(loss="categorical_crossentropy", optimizer="sgd")
+    # unet.compile(loss="categorical_crossentropy", optimizer="sgd")
+    # vgg_model.compile(loss="categorical_crossentropy", optimizer="sgd")
+    print("Done compiling")
+    print("model input shape", model.input_shape)
+    print("model output shape", model.output_shape)
+    # print(unet.input_shape)
+    # print(unet.output_shape)
+    # print("vgg", vgg_model.input_shape)
+    # print("vgg", vgg_model.output_shape)
+    # exit(0)
+    # labels = labels.squeeze()
+
     args = get_arguments()
     scan_reader = ScanReader(args.data_dir)
     # Get input and output
     # scans = [samples, channels, depth, height, width]
     # labels = [samples, depth, height, width]
     scans, labels = scan_reader.scan_img, scan_reader.label_img
-    validation_scans, validation_labels = scan_reader.validation_scans, scan_reader.validation_labels
+    # validation_scans, validation_labels = scan_reader.validation_scans, scan_reader.validation_labels
     # Reshape input to fit Tensorflow backend
     # [samples, depth, height, width, channel]
-    scans = tf.expand_dims(scans, -1)
-    validation_scans = tf.expand_dims(validation_scans, -1)
-    print(scans)
-    print(labels)
-    print(validation_scans)
-    print(validation_labels)
+    # print(type(scans))
+    scans = np.expand_dims(np.array(scans), -1)
+    # print(scans.shape)
+    f = open("whole_tumor_label.json", "r")
+    l = json.load(f)
+    # exit(0)
+    label = []
+    for key in l:
+        label = [w for (_, w) in l[key]]
+
+    labs = label + label
+    labs = labs + label
+    labs = labs + label
+    # print(len(labs))
+    label = np.array(labs)
+    label = np.reshape(label, (960, ))
+    # print(label)
+    print(label.shape)
+    # labels = np.expand_dims(np.array(labels), -1)
+    # labels = np.expand_dims(np.array(labels), 0)
+    # scans = tf.expand_dims(scans, -1)
+    # labels = tf.expand_dims(labels, -1)
+    # validation_scans = tf.expand_dims(validation_scans, -1)
+    # validation_labels = tf.expand_dims(validation_labels, -1)
+    # print(scans.shape)
+    # print(labels.shape)
+    # print(validation_scans)
+    # print(validation_labels)
     # exit(0)
 
-    # Build ResNet-101 model
-    model = ResnetBuilder.build_resnet_101((240, 155, 1), 1)
+    labels_binary = []
+    row_binary = []
+    # for l in labels:
+    #     print(l.shape)
+    #     onehot = to_categorical(l, num_classes=5)
+    #     print(onehot)
+    #     print(onehot.shape)
+    #     exit(0)
+    #     labels_binary.append(onehot)
+    # for row in l:
+    #     row = to_categorical(row, 5)
+    #     row_binary.append(row)
+    # labels_binary.append(row_binary)
+    # row_binary = []
+    # print(labels_binary[60])
+    # labels_binary = np.array(labels_binary)
+    # labels_binary = to_categorical(labels, num_classes=5)
+    # print(labels_binary[84][90])
+    # print(labels_binary.shape)
+    # exit(0)
+    # class_label = np.random.randint(5, size=(5, 5))
 
-    # Train
-    model.compile(loss="categorical_crossentropy", optimizer="sdg")
-    model.fit(scans, labels)
+    onehot_labels = to_categorical(label, num_classes=2)
+    # onehot_labels = np.expand_dims(onehot_labels, 0)
+    print(type(onehot_labels))
+    # print(class_label)
+    # print(onehot_labels[58])
+    # print(len(onehot_labels))
+    print(onehot_labels.shape)
+    # exit(0)
+    # model.fit(scans, label, epochs=10, steps_per_epoch=1000)
+    # model.fit(scans, labels_binary, epochs=10, steps_per_epoch=1000)
+    model.fit(scans, onehot_labels, epochs=10, steps_per_epoch=1000)
+    # print("Done training")
+    # prediction = model.predict(validation_scans)
+    # print(prediction.shape())
+    # print(len(prediction))
 
 
 if __name__ == '__main__':
