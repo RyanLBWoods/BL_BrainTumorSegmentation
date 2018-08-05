633b6cbad80c59f41962b99f4565b7613a945162
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-08-05 17:06:41 +0100

    add checkpoint

--- train2d.py
+++ train2d.py
@@ -14,7 +14,7 @@ import nibabel as nib
 from keras import optimizers
 from keras.utils import to_categorical
 from utils import batch_generator
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, ModelCheckpoint
 
 n_classes = 5
 BATCH_SIZE = 10
@@ -41,28 +41,6 @@ def get_arguments():
     return parser.parse_args()
 
 
-# def load_data(path_list):
-#     scans = []
-#     for path in path_list:
-#         scan_data = nib.load(path).get_data()
-#         scans.extend(scan_data)
-#     scans = np.expand_dims(scans, -1).astype(np.float32)
-#     return scans
-
-
-# def batch_generator(dict, batch_size):
-#     while True:
-#         for key in dict:
-#             seg_data = nib.load(key).get_data()
-#             y = to_categorical(seg_data, 5)
-#             for value in dict[key]:
-#                 if 'nii.gz' in value:
-#                     slice_data = nib.load(value).get_data()
-#                     x = np.expand_dims(slice_data, -1).astype(np.float32)
-#                     for i in range(0, len(x), batch_size):
-#                         yield (x[i:i + batch_size], y[i:i + batch_size])
-
-
 def main():
     # Get arguments
     args = get_arguments()
@@ -72,14 +50,14 @@ def main():
     model = ResnetBuilder.build_resnet_101((240, 155, 4), 5)
 
     # Set learning rate
-    # sgd = optimizers.SGD(lr=args.learning_rate, momentum=0.9, decay=0, nesterov=False)
+    sgd = optimizers.SGD(lr=args.learning_rate, momentum=0.9, decay=0, nesterov=False)
     # adam = optimizers.Adam(lr=args.learning_rate)
     # Compiling
     print("Compiling...")
     model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
     print(model.summary())
-    with open('model_summary_channels=4.txt', 'w') as ms:
-        model.summary(print_fn=lambda x: ms.write(x + '\n'))
+    # with open('model_summary_channels=4.txt', 'w') as ms:
+    #     model.summary(print_fn=lambda x: ms.write(x + '\n'))
 
     # Get input and output
     print("Reading data...")
@@ -89,24 +67,19 @@ def main():
     # Train the model
     print("Training...")
     tensorboard = TensorBoard(log_dir='./log', histogram_freq=0, write_graph=True, write_images=False)
-    # history = model.fit_generator(generator=batch_generator(train_dict, args.batch_size),
+    checkpoint_path = 'weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5'
+    checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_acc', save_best_only=True, mode='max', verbose=1)
+    # history = model.fit_generator(generator=batch_generator(train_dict, args.batch_size, n_classes),
     #                               epochs=args.nb_epoch, steps_per_epoch=args.steps_per_epoch)
     history = model.fit_generator(generator=batch_generator(train_dict, args.batch_size, n_classes),
                                   epochs=args.nb_epoch, steps_per_epoch=args.steps_per_epoch,
                                   validation_data=batch_generator(validation_dict, args.batch_size, n_classes),
-                                  validation_steps=args.validation_steps, callbacks=tensorboard)
+                                  validation_steps=args.validation_steps, callbacks=[tensorboard, checkpoint])
     print("Done...")
     print("Saving Model...")
     model.save(args.label_class + "_adam_seg_channel=4.h5")
     with open('log_adam_seg_channel=4.txt', 'w') as adam_log:
         adam_log.write(str(history.history))
-
-    #
-    # print("Evaluating...")
-    # error = model.evaluate_generator(generator=batch_generator(validation_dict, args.label_class, args.batch_size),
-    #                                  steps=args.validation_steps)
-    # with open('log_adam_val_seg.txt', 'w') as val_error:
-    #     val_error.write(str(error))
 
 
 if __name__ == '__main__':
